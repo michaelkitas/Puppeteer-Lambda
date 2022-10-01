149dc61af86748a4ad243c81f29619cd3d555a04
Michael Kitas <mixaliskitas@gmail.com>
2022-10-01 17:25:10 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,30 @@
+const chromium = require('chrome-aws-lambda');
+
+exports.handler = async (event, context, callback) => {
+  let result = null;
+  let browser = null;
+
+  try {
+    browser = await chromium.puppeteer.launch({
+      args: chromium.args,
+      defaultViewport: chromium.defaultViewport,
+      executablePath: await chromium.executablePath,
+      headless: chromium.headless,
+      ignoreHTTPSErrors: true,
+    });
+
+    let page = await browser.newPage();
+
+    await page.goto(event.url || 'https://example.com');
+
+    result = await page.title();
+  } catch (error) {
+    return callback(error);
+  } finally {
+    if (browser !== null) {
+      await browser.close();
+    }
+  }
+
+  return callback(null, result);
+};
